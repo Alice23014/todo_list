73f58dd6992f4b570ecc7742181a88a6bf1b9c39
Alice <alice@gmail.com>
2024-11-02 18:02:27 +0300

    end to do app

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,56 +1,81 @@
-import { create } from 'zustand'
-import { generateId } from '../helpers';
+import { create } from "zustand";
+import { State, StateCreator } from "zustand";
+import { devtools } from "zustand/middleware";
+import { generateId } from "../helpers";
 
-interface Task{
-    id:string;
-    title: string;
-    createdAt: number;
+interface Task {
+  id: string;
+  title: string;
+  createdAt: number;
 }
 
-interface ToDoStore{
-    tasks: Task[];
-    createTask: (title: string) => void;
-    removeTask: (id: string) => void;
-    updateTask: (id: string, title: string) => void;
+interface ToDoStore {
+  tasks: Task[];
+  createTask: (title: string) => void;
+  removeTask: (id: string) => void;
+  updateTask: (id: string, title: string) => void;
 }
 
-export const useToDoStore = create<ToDoStore>((set, get)=>({
-    tasks: [
-        {
-        id: '1',
-        title: 'Task 1',
-        createdAt: 423423,
-        },
-        {
-        id: '2',
-        title: 'Task 2',
-        createdAt: 2423525,
-        }
-    ],
-    createTask: (title) => {
-        const{tasks} = get();
-        const newTask ={
+const persistMiddleware =
+  <T extends State>(config: StateCreator<T>): StateCreator<T> =>
+  (set, get, api) =>
+    config(
+      (nextState) => {
+        set(nextState);
+        localStorage.setItem("todoStore", JSON.stringify(get()));
+      },
+      get,
+      api
+    );
+
+export const useToDoStore = create<ToDoStore>()(
+  devtools(
+    persistMiddleware((set, get) => {
+      const storedTasks = localStorage.getItem("todoStore");
+      const initialTasks = storedTasks
+        ? JSON.parse(storedTasks).tasks
+        : [
+            {
+              id: "1",
+              title: "Task 1",
+              createdAt: 423423,
+            },
+            {
+              id: "2",
+              title: "Task 2",
+              createdAt: 2423525,
+            },
+          ];
+
+      return {
+        tasks: initialTasks,
+        createTask: (title) => {
+          const { tasks } = get();
+          const newTask = {
             id: generateId(),
             title,
             createdAt: Date.now(),
-        }
-        set({
-            tasks: [newTask].concat(tasks),
-        })
-    },
-    removeTask:(id:string) =>{
-        const { tasks } = get();
-        set({
-            tasks: tasks.filter((task) => task.id !== id)
-        })
-    },
-    updateTask: (id:string, title:string) => {
-        const {tasks} = get();
-        set({
+          };
+          set({
+            tasks: [newTask, ...tasks],
+          });
+        },
+        removeTask: (id: string) => {
+          const { tasks } = get();
+          set({
+            tasks: tasks.filter((task) => task.id !== id),
+          });
+        },
+        updateTask: (id: string, title: string) => {
+          const { tasks } = get();
+          set({
             tasks: tasks.map((task) => ({
-                ...task,
-                title: task.id === id ? title : task.title,
-            }))
-        })
-    },
-}))
+              ...task,
+              title: task.id === id ? title : task.title,
+            })),
+          });
+        },
+      };
+    })
+  )
+);
--- src/views/components/InputTask/index.tsx
+++ src/views/components/InputTask/index.tsx
@@ -1,5 +1,4 @@
-import React, {useState} from "react"; 
-import { useToDoStore } from "../../data/stores/useToDoStore"; 
+import React, { useState, useRef, useEffect } from "react"; 
 import styles from "./index.module.scss"; 
 
 interface InputTaskProps {
@@ -16,34 +15,75 @@ export const InputTask: React.FC<InputTaskProps> = (
     const [checked, setChecked] = useState(false);
     const [isEditMode, setIsEditMode] = useState(false);
     const [value, setValue] = useState(title);
+    const editTitleInputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() =>{
+        if(isEditMode){
+            editTitleInputRef?.current?.focus();
+        }
+    }, [isEditMode])
 
     return (
-        <div className={styles.inputTask}>
-            <label className={styles.inputTaskLabel}>
-            <input 
-            type='checkbox' 
+      <div className={styles.inputTask}>
+        <label className={styles.inputTaskLabel}>
+          <input
+            type="checkbox"
             disabled={isEditMode}
-            checked={checked} 
-            className={styles.inputTaskCheckbox} 
-            onChange={(e)=>{
-                setChecked(e.target.checked)
-                if(e.target.checked){
-                    onDone(id)
+            checked={checked}
+            className={styles.inputTaskCheckbox}
+            onChange={(e) => {
+              setChecked(e.target.checked);
+              if (e.target.checked) {
+                onDone(id);
+              }
+            }}
+          ></input>
+          {isEditMode ? (
+            <input
+              value={value}
+              ref = {editTitleInputRef}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  onEdited(id, value);
+                  setIsEditMode(false);
                 }
-                }}>
-            </input> 
-            {isEditMode ? (
-                <input value={value} onChange={(e)=>{setValue(e.target.value)}} className={styles.inputTaskTitleEdit}></input>
-            ): (<h3 className={styles.inputTaskTitle}>{title}</h3>)}
-           </label>
-           {isEditMode ? (<button aria-label='Save' className={styles.inputTaskSave} onKeyDown={(e)=> {if(e.key=== "Enter"){onEdited(id, value); setIsEditMode(false)}}} onClick={()=>{onEdited(id, value); setIsEditMode(false)}}></button>) : (
-               <button aria-label='Edit' className={styles.inputTaskEdit} onClick={()=>{setIsEditMode(true)}}></button>
-           )}
-            <button aria-label='Remove' className={styles.inputTaskRemove} onClick={()=>{
-                if(confirm('Are you sure?')){
-                    onRemoved(id)
-                }
-            }}></button>
-        </div>
-    )
+              }}
+              className={styles.inputTaskTitleEdit}
+            ></input>
+          ) : (
+            <h3 className={styles.inputTaskTitle}>{title}</h3>
+          )}
+        </label>
+        {isEditMode ? (
+          <button
+            aria-label="Save"
+            className={styles.inputTaskSave}
+            onClick={() => {
+              onEdited(id, value);
+              setIsEditMode(false);
+            }}
+          ></button>
+        ) : (
+          <button
+            aria-label="Edit"
+            className={styles.inputTaskEdit}
+            onClick={() => {
+              setIsEditMode(true);
+            }}
+          ></button>
+        )}
+        <button
+          aria-label="Remove"
+          className={styles.inputTaskRemove}
+          onClick={() => {
+            if (confirm("Are you sure?")) {
+              onRemoved(id);
+            }
+          }}
+        ></button>
+      </div>
+    );
 }
